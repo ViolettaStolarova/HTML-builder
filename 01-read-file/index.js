91e9723fe99c2a6481d8cbe60d1d9df1b4640bb8
Violetta <violettastolarova@icloud.com>
2024-01-23 01:56:38 +0300

    feat: complete 01-read-file task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 
-let filePath = path.join(__dirname, 'text.txt');
-let readStream = fs.createReadStream(filePath, 'utf-8');
+const filePath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(filePath, 'utf-8');
+
 readStream.on('data', (chunk) => console.log(chunk));
