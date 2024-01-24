146afd6c645260606f1779ebc4c634733397819e
Violetta <violettastolarova@icloud.com>
2024-01-24 14:43:06 +0300

    feat: add file deletion from copy-files

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -13,6 +13,7 @@ async function copyDir() {
   }
 
   const files = await fs.readdir(pathToFiles, { withFileTypes: true });
+  const copyFiles = await fs.readdir(pathToCopyFiles, { withFileTypes: true });
 
   for (const file of files) {
     if (!file.isDirectory()) {
@@ -22,6 +23,19 @@ async function copyDir() {
         await fs.copyFile(fromWhere, toWhere);
       } catch (err) {
         console.log(err);
+        process.exit();
+      }
+    }
+  }
+
+  for (const copyFile of copyFiles) {
+    if (!files.find((file) => file.name === copyFile.name)) {
+      const copyFilePath = path.join(pathToCopyFiles, copyFile.name);
+      try {
+        await fs.unlink(copyFilePath);
+      } catch (err) {
+        console.log(err);
+        process.exit();
       }
     }
   }
